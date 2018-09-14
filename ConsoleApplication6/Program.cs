48bcdb458c8b5e9c287154433bad454dcb7c95ae
Peter Zaporowski <peterzaporowski@Peters-Mac.local>
2018-09-14 21:52:50 +0200

    Initial commit

---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double myDouble = 13.37;
+            float myFloat = 13.37F;
+            double myNewDouble = myFloat;
+
+            int myInt = (int)myDouble;
+            string myString = myDouble.ToString();
+            string myFloatString = myFloat.ToString();
+
+            Console.WriteLine("Integer value: ");
+            Console.WriteLine(myInt);
+            Console.WriteLine("\n");
+            Console.WriteLine("Float value: ");
+            Console.WriteLine(myFloat);
+            Console.WriteLine("\n");
+            Console.WriteLine("Double value: ");
+            Console.WriteLine(myDouble);
+            Console.WriteLine("\n");
+            Console.WriteLine("Double value: (from Float) ");
+            Console.WriteLine(myNewDouble);
+            Console.WriteLine("\n");
+            Console.WriteLine("String from double: ");
+            Console.WriteLine(myString);
+            Console.WriteLine("\n");
+            Console.WriteLine("String from float: ");
+            Console.WriteLine(myFloatString);
+            Console.Read();
+
+        }
+    }
+}
